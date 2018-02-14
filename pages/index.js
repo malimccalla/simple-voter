f0fbdde67f8d001daf49789e8655a91a4d67e77f
Mali Michael <malimccalla@gmail.com>
2018-02-14 08:10:22 +0000

    Configure styled-components with custom document

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,48 @@
-export default () => <h2>Simple voter</h2>;
+import styled, { injectGlobal } from 'styled-components';
+
+export default () => <App>Simple voter</App>;
+
+const App = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 6px;
+  background-color: #f1f1f1;
+
+  height: calc(100vh - 6rem);
+  width: calc(100vw - 6rem);
+
+  margin: 3rem auto;
+`;
+
+injectGlobal`
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    vertical-align: middle;
+    font-family: inherit;
+    box-sizing: inherit;
+    font-weight: inherit;
+  }
+  ul {
+    list-style: none;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  button {
+    font-family: sans-serif;
+    font-size: 1.6rem;
+  }
+  body {
+    color: ${props => props.theme.black};
+    font-family: sans-serif;
+    font-size: 1.6rem;
+    font-weight: 600;
+    letter-spacing: 0.4px;
+    box-sizing: border-box;
+  }
+`;
