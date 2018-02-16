5d0f75e62f7330b360afa3c2e804d46295f62ece
Mali Michael <malimccalla@gmail.com>
2018-02-16 20:23:09 +0000

    Adds scripts and test set up

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import styled, { injectGlobal } from 'styled-components';
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 6px;
+  background-color: #f1f1f1;
+
+  height: calc(100vh - 6rem);
+  width: calc(100vw - 6rem);
+
+  margin: 3rem auto;
+`;
+
+injectGlobal`
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    vertical-align: middle;
+    font-family: inherit;
+    box-sizing: inherit;
+    font-weight: inherit;
+  }
+  ul {
+    list-style: none;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  button {
+    font-family: sans-serif;
+    font-size: 1.6rem;
+  }
+  body {
+    color: #111111;
+    font-family: sans-serif;
+    font-size: 1.6rem;
+    font-weight: 600;
+    letter-spacing: 0.4px;
+    box-sizing: border-box;
+  }
+`;
---
+++ ethereum/test/SimpleVoter.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('SimpleVoter', () => {
+  it('runs tests', () => {
+    assert.ok(true);
+  });
+});
