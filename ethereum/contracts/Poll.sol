920ae7f94c1a7f3b7fbda2c70e29a549d1a68e29
Mali Michael <malimccalla@gmail.com>
2018-02-17 18:33:35 +0000

    Add getDetails function

--- ethereum/contracts/Poll.sol
+++ ethereum/contracts/Poll.sol
@@ -16,9 +16,9 @@ contract PollFactory {
 contract Poll {
     mapping(address => bool) public voters;
     address public creator;
+    string public question;
     uint public yesVotesCount = 0;
     uint public noVotesCount = 0;
-    string public question;
 
     function Poll(string _question, address _creator) public {
         question = _question;
@@ -37,5 +37,9 @@ contract Poll {
 
     function voteNo() public notVoted {
         noVotesCount++;
+    }
+
+    function getDetails() public view returns (address, string, uint, uint) {
+      return (creator, question, yesVotesCount, noVotesCount);
     }
 }
