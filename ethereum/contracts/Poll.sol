a6ac3f533dc7259c88274500a7e97ba35a00e17f
Mali Michael <malimccalla@gmail.com>
2018-02-18 09:45:26 +0000

    Update contract to be able to set button text and return if voted in getDetails

--- ethereum/contracts/Poll.sol
+++ ethereum/contracts/Poll.sol
@@ -1,10 +1,16 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.20;
 
 contract PollFactory {
     address[] public deployedPolls;
 
-    function createPoll(string question) public {
-        address newPoll = new Poll(question, msg.sender);
+    function createPoll
+    (string question, string yesButtonText, string noButtonText) public {
+        address newPoll = new Poll(
+            msg.sender,
+            question,
+            yesButtonText,
+            noButtonText
+        );
         deployedPolls.push(newPoll);
     }
 
@@ -17,12 +23,24 @@ contract Poll {
     mapping(address => bool) public voters;
     address public creator;
     string public question;
-    uint public yesVotesCount = 0;
-    uint public noVotesCount = 0;
+    string public yesButtonText;
+    string public noButtonText;
+    uint public yesVotesCount;
+    uint public noVotesCount;
 
-    function Poll(string _question, address _creator) public {
-        question = _question;
+    function Poll
+    (
+        address _creator,
+        string _question,
+        string _yesButtonText,
+        string _noButtonText
+    ) public {
         creator = _creator;
+        question = _question;
+        yesButtonText = _yesButtonText;
+        noButtonText = _noButtonText;
+        yesVotesCount = 0;
+        noVotesCount = 0;
     }
 
     modifier notVoted() {
@@ -39,7 +57,17 @@ contract Poll {
         noVotesCount++;
     }
 
-    function getDetails() public view returns (address, string, uint, uint) {
-      return (creator, question, yesVotesCount, noVotesCount);
+    function getDetails() public view returns
+    (address, string, string, string, uint, uint, bool) {
+        bool hasVoted = voters[msg.sender];
+        return (
+            creator,
+            question,
+            yesButtonText,
+            noButtonText,
+            yesVotesCount,
+            noVotesCount,
+            hasVoted
+        );
     }
 }
